86ef172602d177aa40da454b054fa056d382663b
quh4gko8 <88831734+quh4gko8@users.noreply.github.com>
2025-04-02 16:22:14 +0000

    Update utility methods with some full users supporting some profile types
    Secondary users now can have private profiles as its nested profile.
    
    Signed-off-by: Dmitrii <bankersenator@gmail.com>

--- src/com/android/settings/Utils.java
+++ src/com/android/settings/Utils.java
@@ -459,6 +459,12 @@ public final class Utils extends com.android.settingslib.Utils {
             if (Objects.equals(umUserType, userInfo.userType)) {
                 return profile;
             }
+            if (userType == ProfileType.PERSONAL) {
+                if (userInfo.canHaveProfileOfType(USER_TYPE_PROFILE_PRIVATE)
+                        || userInfo.canHaveProfileOfType(USER_TYPE_PROFILE_MANAGED)) {
+                    return profile;
+                }
+            }
         }
         return null;
     }
@@ -474,6 +480,12 @@ public final class Utils extends com.android.settingslib.Utils {
         for (UserInfo profile : userProfiles) {
             if (Objects.equals(umUserType, profile.userType)) {
                 return true;
+            }
+            if (userType == ProfileType.PERSONAL) {
+                if (profile.canHaveProfileOfType(USER_TYPE_PROFILE_PRIVATE)
+                        || profile.canHaveProfileOfType(USER_TYPE_PROFILE_MANAGED)) {
+                    return true;
+                }
             }
         }
         return false;
